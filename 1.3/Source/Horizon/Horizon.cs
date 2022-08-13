5a3025008e6ce050dd3f87dcb2452bce9761ef26
Taranchuk <nfhfyxer@gmail.com>
2022-08-13 10:15:59 +0300

    update

--- 1.3/Source/Horizon/Horizon.cs
+++ 1.3/Source/Horizon/Horizon.cs
@@ -12,6 +12,8 @@ using UnityEngine;
 using Verse.AI.Group;
 using static Verse.DamageWorker;
 using MonoMod.Utils;
+using static AlienRace.AlienPartGenerator;
+using AlienRace;
 
 namespace Horizon
 {
@@ -554,6 +556,303 @@ namespace Horizon
                 return false;
             }
             return true;
+        }
+    }
+    [HarmonyPatch(typeof(HarmonyPatches), nameof(HarmonyPatches.DrawAddons))]
+    public static class HarmonyPatches_DrawAddons_Patch
+    {
+        public static void Postfix(PawnRenderFlags renderFlags, Vector3 vector, Vector3 headOffset, Pawn pawn, Quaternion quat, Rot4 rotation)
+        {
+            var extension = pawn.def.GetModExtension<AnimalBodyAddons>();
+            if (extension == null)
+            {
+                return;
+            }
+            Log.Message("Drawing body addons: " + extension);
+            List<AlienPartGenerator.BodyAddon> bodyAddons = extension.bodyAddons;
+            var comp = pawn.GetComp<AnimalComp>();
+            if (comp == null)
+            {
+                return;
+            }
+            bool flag = renderFlags.FlagSet(PawnRenderFlags.Portrait);
+            bool flag2 = renderFlags.FlagSet(PawnRenderFlags.Invisible);
+            for (int i = 0; i < bodyAddons.Count; i++)
+            {
+                AlienPartGenerator.BodyAddon bodyAddon = bodyAddons[i];
+                Log.Message("1 Drawing addon: " + bodyAddon);
+                if (!bodyAddon.CanDrawAddon(pawn))
+                {
+                    continue;
+                }
+                Log.Message("Drawing addon: " + bodyAddon);
+                Vector3 v = (bodyAddon.defaultOffsets.GetOffset(rotation)?.GetOffset(flag, BodyTypeDefOf.Male, "") ?? Vector3.zero) + (bodyAddon.offsets.GetOffset(rotation)?.GetOffset(flag, BodyTypeDefOf.Male, "") ?? Vector3.zero);
+                v.y = (bodyAddon.inFrontOfBody ? (0.3f + v.y) : (-0.3f - v.y));
+                float num = bodyAddon.angle;
+                if (rotation == Rot4.North)
+                {
+                    if (bodyAddon.layerInvert)
+                    {
+                        v.y = 0f - v.y;
+                    }
+                    num = 0f;
+                }
+                if (rotation == Rot4.East)
+                {
+                    num = 0f - num;
+                    v.x = 0f - v.x;
+                }
+                Graphic graphic = comp.addonGraphics[i];
+                graphic.drawSize = ((flag && bodyAddon.drawSizePortrait != Vector2.zero) ? bodyAddon.drawSizePortrait : bodyAddon.drawSize) * ((!bodyAddon.scaleWithPawnDrawsize) ? Vector2.one : ((!bodyAddon.alignWithHead) ? (flag ? comp.customPortraitDrawSize : comp.customDrawSize) : (flag ? comp.customPortraitHeadDrawSize : comp.customHeadDrawSize))) * 1.5f;
+                Material material = graphic.MatAt(rotation);
+                if (!flag && flag2)
+                {
+                    material = InvisibilityMatPool.GetInvisibleMat(material);
+                }
+                GenDraw.DrawMeshNowOrLater(graphic.MeshAt(rotation), vector + (bodyAddon.alignWithHead ? headOffset : Vector3.zero) + v.RotatedBy(Mathf.Acos(Quaternion.Dot(Quaternion.identity, quat)) * 2f * 57.29578f), Quaternion.AngleAxis(num, Vector3.up) * quat, material, renderFlags.FlagSet(PawnRenderFlags.DrawNow));
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(HarmonyPatches), nameof(HarmonyPatches.ResolveAllGraphicsPrefix))]
+    public static class HarmonyPatches_ResolveAllGraphicsPrefix_Patch
+    {
+        public static void Prefix(PawnGraphicSet __0)
+        {
+            var pawn = __0.pawn;
+            if (!(pawn.def is AlienRace.ThingDef_AlienRace))
+            {
+                var comp = pawn.GetComp<AnimalComp>();
+                if (comp != null)
+                {
+                    var extension = pawn.def.GetModExtension<AnimalBodyAddons>();
+                    if (extension != null)
+                    {
+                        comp.addonGraphics = new List<Graphic>();
+                        if (comp.addonVariants == null)
+                        {
+                            comp.addonVariants = new List<int>();
+                        }
+                        int sharedIndex = 0;
+                        for (int i = 0; i < extension.bodyAddons.Count; i++)
+                        {
+                            Graphic path = extension.bodyAddons[i].GetPath(pawn, ref sharedIndex, (comp.addonVariants.Count > i) ? new int?(comp.addonVariants[i]) : null);
+                            comp.addonGraphics.Add(path);
+                            if (comp.addonVariants.Count <= i)
+                            {
+                                comp.addonVariants.Add(sharedIndex);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class HARHandler
+    {
+        static HARHandler()
+        {
+            foreach (var def in DefDatabase<ThingDef>.AllDefs)
+            {
+                var extension = def.GetModExtension<AnimalBodyAddons>();
+                if (extension != null)
+                {
+                    extension.GenerateMeshsAndMeshPools(def);
+                    def.comps.Add(new CompProperties(typeof(AnimalComp)));
+                }
+            }
+        }
+    }
+
+    public class AnimalBodyAddon : BodyAddon
+    {
+        public override Graphic GetPath(Pawn pawn, ref int sharedIndex, int? savedIndex = null)
+        {
+            string returnPath = string.Empty;
+            int variantCounting = 0;
+
+            if (!this.hediffGraphics.NullOrEmpty())
+                foreach (BodyAddonHediffGraphic bahg in this.hediffGraphics)
+                {
+
+                    foreach (Hediff h in pawn.health.hediffSet.hediffs.Where(predicate: h => h.def == bahg.hediff &&
+                                                                                             (h.Part == null ||
+                                                                                              this.bodyPart.NullOrEmpty() ||
+                                                                                              h.Part.untranslatedCustomLabel == this.bodyPart ||
+                                                                                              h.Part.def.defName == this.bodyPart)))
+                    {
+                        returnPath = bahg.path;
+                        variantCounting = bahg.variantCount;
+
+                        if (!bahg.severity.NullOrEmpty())
+                            foreach (BodyAddonHediffSeverityGraphic bahsg in bahg.severity)
+                            {
+                                if (h.Severity >= bahsg.severity)
+                                {
+                                    returnPath = bahsg.path;
+                                    variantCounting = bahsg.variantCount;
+                                    break;
+                                }
+                            }
+                        break;
+                    }
+                }
+
+            if (returnPath.NullOrEmpty())
+            {
+                returnPath = this.path;
+                variantCounting = this.variantCount;
+            }
+
+            if (variantCounting <= 0)
+                variantCounting = 1;
+            int tv;
+
+            return !returnPath.NullOrEmpty() ?
+                       GraphicDatabase.Get<Graphic_Multi_RotationFromData>(returnPath += (tv = (savedIndex.HasValue ? (sharedIndex = savedIndex.Value % variantCounting) :
+                                                                                         (this.linkVariantIndexWithPrevious ?
+                                                                                              sharedIndex % variantCounting :
+                                                                                              (sharedIndex = Rand.Range(min: 0, variantCounting))))) == 0 ? "" : tv.ToString(),
+                                                          ContentFinder<Texture2D>.Get(returnPath + "_northm", reportFailure: false) == null ? this.ShaderType.Shader : ShaderDatabase.CutoutComplex, //ShaderDatabase.Transparent,
+                                                          this.drawSize * 1.5f,
+                                                          Color.white, Color.white, new GraphicData
+                                                          {
+                                                              drawRotated = !this.drawRotated
+                                                          }) :
+                       null;
+        }
+    }
+    public class AnimalComp : ThingComp
+    {
+        public List<Graphic> addonGraphics;
+        public List<int> addonVariants;
+        public Vector2 customDrawSize = Vector2.one;
+
+        public Vector2 customHeadDrawSize = Vector2.one;
+
+        public Vector2 customPortraitDrawSize = Vector2.one;
+
+        public Vector2 customPortraitHeadDrawSize = Vector2.one;
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Collections.Look(ref addonVariants, "addonVariants", LookMode.Undefined);
+        }
+    }
+    public class AnimalBodyAddons : DefModExtension
+    {
+        public List<BodyAddon> bodyAddons = new List<BodyAddon>();
+        public List<OffsetNamed> offsetDefaults = new List<OffsetNamed>();
+        public void GenerateMeshsAndMeshPools(ThingDef def)
+        {
+            offsetDefaults.Add(new OffsetNamed
+            {
+                name = "Center",
+                offsets = new BodyAddonOffsets()
+            });
+            offsetDefaults.Add(new OffsetNamed
+            {
+                name = "Tail",
+                offsets = new BodyAddonOffsets
+                {
+                    south = new RotationOffset
+                    {
+                        offset = new Vector2(0.42f, -0.22f)
+                    },
+                    north = new RotationOffset
+                    {
+                        offset = new Vector2(0f, -0.55f)
+                    },
+                    east = new RotationOffset
+                    {
+                        offset = new Vector2(0.42f, -0.22f)
+                    },
+                    west = new RotationOffset
+                    {
+                        offset = new Vector2(0.42f, -0.22f)
+                    }
+                }
+            });
+            offsetDefaults.Add(new OffsetNamed
+            {
+                name = "Head",
+                offsets = new BodyAddonOffsets
+                {
+                    south = new RotationOffset
+                    {
+                        offset = new Vector2(0f, 0.5f)
+                    },
+                    north = new RotationOffset
+                    {
+                        offset = new Vector2(0f, 0.35f)
+                    },
+                    east = new RotationOffset
+                    {
+                        offset = new Vector2(-0.07f, 0.5f)
+                    },
+                    west = new RotationOffset
+                    {
+                        offset = new Vector2(-0.07f, 0.5f)
+                    }
+                }
+            });
+            StringBuilder logBuilder = new StringBuilder();
+            bodyAddons.Do(delegate (BodyAddon ba)
+            {
+                ba.defaultOffsets = offsetDefaults.Find((OffsetNamed on) => on.name == ba.defaultOffset).offsets;
+                if (ba.variantCount == 0)
+                {
+                    AddToStringBuilder("Loading variants for " + ba.path);
+                    while (ContentFinder<Texture2D>.Get(ba.path + ((ba.variantCount == 0) ? "" : ba.variantCount.ToString()) + "_north", reportFailure: false) != null)
+                    {
+                        ba.variantCount++;
+                    }
+                    AddToStringBuilder($"Variants found for {ba.path}: {ba.variantCount}");
+                    if (ba.hediffGraphics != null)
+                    {
+                        foreach (BodyAddonHediffGraphic item2 in ba.hediffGraphics.Where((BodyAddonHediffGraphic bahg) => bahg.variantCount == 0))
+                        {
+                            while (ContentFinder<Texture2D>.Get(item2.path + ((item2.variantCount == 0) ? "" : item2.variantCount.ToString()) + "_north", reportFailure: false) != null)
+                            {
+                                item2.variantCount++;
+                            }
+                            AddToStringBuilder($"Variants found for {item2.path}: {item2.variantCount}");
+                            if (item2.variantCount == 0)
+                            {
+                                Log.Warning($"No hediff graphics found at {item2.path} for hediff {item2.hediff} in {def}");
+                            }
+                            if (item2.severity != null)
+                            {
+                                foreach (BodyAddonHediffSeverityGraphic item3 in item2.severity)
+                                {
+                                    while (ContentFinder<Texture2D>.Get(item3.path + ((item3.variantCount == 0) ? "" : item3.variantCount.ToString()) + "_north", reportFailure: false) != null)
+                                    {
+                                        item3.variantCount++;
+                                    }
+                                    AddToStringBuilder($"Variants found for {item3.path} at severity {item3.severity}: {item3.variantCount}");
+                                    if (item3.variantCount == 0)
+                                    {
+                                        Log.Warning($"No hediff graphics found at {item3.path} at severity {item3.severity} for hediff {item2.hediff} in {def}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                void AddToStringBuilder(string s)
+                {
+                    if (ba.debug)
+                    {
+                        logBuilder.AppendLine(s);
+                    }
+                }
+            });
+            if (logBuilder.Length > 0)
+            {
+                Log.Message($"Loaded body addon variants for {def} \n{logBuilder}");
+            }
         }
     }
 }
